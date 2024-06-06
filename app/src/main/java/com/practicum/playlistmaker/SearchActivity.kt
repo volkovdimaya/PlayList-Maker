6132dddf6e83b65a7ef52d007212774aa590dcea
volkovdimaya <volkov.dimaya@yandex.ru>
2024-06-06 11:17:05 +0500

    для работы в др месте

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,31 +1,34 @@
 package com.practicum.playlistmaker
 
+import android.content.ContentValues.TAG
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
 class SearchActivity : AppCompatActivity() {
+
+    private var textSearch : String = ""
+    private lateinit var search : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-
+        search = findViewById(R.id.search)
         val toolbar: Toolbar = findViewById(R.id.toolbar_search)
         val clearButton : ImageView = findViewById(R.id.clearIcon)
-        val search : EditText = findViewById(R.id.search)
-
 
         toolbar.setNavigationOnClickListener {
             finish()
         }
-
 
 
         val textWatcher = object : TextWatcher{
@@ -38,20 +41,37 @@ class SearchActivity : AppCompatActivity() {
             }
 
             override fun afterTextChanged(s: Editable?) {
-
+                textSearch = search.getText().toString()
             }
         }
         search.addTextChangedListener(textWatcher)
 
         clearButton.setOnClickListener {
             search.setText("")
-            //search.setFocusable(false)
 
             val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
             inputMethodManager?.hideSoftInputFromWindow(`it`.windowToken, 0)
 
         }
 
+        Toast.makeText(this, "$savedInstanceState Данные загружены", Toast.LENGTH_SHORT).show()
+        savedInstanceState?.let {
+            textSearch = it.getString(FIELD_SEARCH).orEmpty()
+            search.setText(textSearch)
+            Toast.makeText(this, "Данные загружены", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        textSearch = savedInstanceState.getString(FIELD_SEARCH).toString()
+
+        search.setText(textSearch)
+
+        Toast.makeText(this, "Данные загружены", Toast.LENGTH_SHORT).show()
+        Log.d(TAG, "onRestoreInstanceState: Данные загружены")
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
@@ -60,5 +80,17 @@ class SearchActivity : AppCompatActivity() {
         } else {
             View.VISIBLE
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(FIELD_SEARCH, textSearch)
+        Toast.makeText(this, "Данные сохранены", Toast.LENGTH_SHORT).show()
+        Log.d(TAG, "onSaveInstanceState: Данные сохранены")
+        search.setText("1111")
+    }
+
+    companion object{
+        const val FIELD_SEARCH = "FIELD_SEARCH"
     }
 }
