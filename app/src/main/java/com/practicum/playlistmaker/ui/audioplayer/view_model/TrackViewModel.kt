54904a467fa32ad577e9fb59bd48aaecd6389559
volkovdimaya <volkov.dimaya@yandex.ru>
2025-02-14 21:41:40 +0500

    19 спринт использовал  libs.versions для подключения библиотек

--- app/src/main/java/com/practicum/playlistmaker/ui/audioplayer/view_model/TrackViewModel.kt
+++ app/src/main/java/com/practicum/playlistmaker/ui/audioplayer/view_model/TrackViewModel.kt
@@ -2,7 +2,6 @@ package com.practicum.playlistmaker.ui.audioplayer.view_model
 
 
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
