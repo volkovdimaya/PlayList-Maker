239d01db0fdafeb660ffdc3b642251b652ad4b8a
volkovdimaya <volkov.dimaya@yandex.ru>
2025-04-27 18:13:59 +0500

    добавлено форматирование количества треков и glide чтобы обновлял изображение при его изменении если он ссылается на файл с одинаковым названием он кэширует

--- app/src/main/java/com/practicum/playlistmaker/ui/audioplayer/bottom_sheet/BottomSheetPlaylistAdapterDelegates.kt
+++ app/src/main/java/com/practicum/playlistmaker/ui/audioplayer/bottom_sheet/BottomSheetPlaylistAdapterDelegates.kt
@@ -3,12 +3,15 @@ package com.practicum.playlistmaker.ui.audioplayer.bottom_sheet
 
 
 import com.bumptech.glide.Glide
+import com.bumptech.glide.signature.ObjectKey
 import com.practicum.playlistmaker.RVItem
 import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateViewBinding
 import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.databinding.ItemPlaylistSheetBinding
 import com.practicum.playlistmaker.ui.audioplayer.bottom_sheet.fragment.PlaylistItemClickListenerBottomSheet
 import com.practicum.playlistmaker.ui.audioplayer.bottom_sheet.models.PlaylistBottomSheetItem
+import com.practicum.playlistmaker.util.TrackCountFormatter
+import java.io.File
 
 
 fun bottomSheetPlaylistDelegate( itemClickListener: PlaylistItemClickListenerBottomSheet) = adapterDelegateViewBinding<PlaylistBottomSheetItem, RVItem, ItemPlaylistSheetBinding>(
@@ -17,7 +20,7 @@ fun bottomSheetPlaylistDelegate( itemClickListener: PlaylistItemClickListenerBot
 
 
     bind {
-        binding.count.text = item.trackCount.toString()
+        binding.count.text = TrackCountFormatter.formatTrackCount(item.trackCount)
         binding.title.text = item.title
 
 
@@ -29,6 +32,7 @@ fun bottomSheetPlaylistDelegate( itemClickListener: PlaylistItemClickListenerBot
             .load(item.image)
             .centerCrop()
             .placeholder(R.drawable.place_holder_cover)
+            .signature(ObjectKey(File(item.image!!.path).lastModified()))
             .into(binding.image)
     }
 }
