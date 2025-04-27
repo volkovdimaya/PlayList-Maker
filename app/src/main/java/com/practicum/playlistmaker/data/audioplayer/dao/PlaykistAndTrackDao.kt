e9740d242aba50235035504b41880257be802162
volkovdimaya <volkov.dimaya@yandex.ru>
2025-04-27 18:04:32 +0500

    добавлены методы для работы с плейлистами: добавление и удаление треков, получение треков из плейлиста и колличество треков в плейлисте

--- app/src/main/java/com/practicum/playlistmaker/data/audioplayer/dao/PlaykistAndTrackDao.kt
+++ app/src/main/java/com/practicum/playlistmaker/data/audioplayer/dao/PlaykistAndTrackDao.kt
@@ -1,12 +1,33 @@
 package com.practicum.playlistmaker.data.audioplayer.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.practicum.playlistmaker.data.audioplayer.entity.PlayListAndTrackEntity
+import com.practicum.playlistmaker.data.audioplayer.entity.PlaylistItemEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PlaylistAndTrackDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun addTrackToPlaylistById(playlistAndTrackEntity: PlayListAndTrackEntity) : Long
+    suspend fun addTrackToPlaylistTablePlayListAndTrack(playlistAndTrackEntity: PlayListAndTrackEntity) : Long
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addTrackToPlaylistItem(playlist: PlaylistItemEntity)
+
+    @Query("SELECT * FROM playlist_item WHERE track_id IN (SELECT track_id FROM playlist_and_track WHERE playlist_id = :id)")
+    fun getTracksInPlaylist(id: Long): Flow<List<PlaylistItemEntity>>
+
+    @Delete
+    suspend fun deleteTrack(playlistItemEntity : PlaylistItemEntity)
+
+    @Query("SELECT COUNT(*) FROM playlist_and_track WHERE track_id = :trackId")
+    suspend fun getTrackPlaylistCount(trackId: String): Int
+
+    @Delete
+    suspend fun deletePlayListAndTrack(playlistAndTrackEntity: PlayListAndTrackEntity)
+
+
 }
