d3c323ccd140f29e7a90ac6159311ee0b60c98a9
volkovdimaya <volkov.dimaya@yandex.ru>
2025-04-27 18:01:51 +0500

    добавлено условие для предотвращения перезаписи существующего изображения и реализована функция удаления изображения

--- app/src/main/java/com/practicum/playlistmaker/data/add_playlist/PicRepositoryImpl.kt
+++ app/src/main/java/com/practicum/playlistmaker/data/add_playlist/PicRepositoryImpl.kt
@@ -6,6 +6,7 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Environment
+import android.util.Log
 import androidx.core.net.toUri
 import com.practicum.playlistmaker.domain.add_playlist.PicRepository
 import java.io.File
@@ -13,32 +14,30 @@ import java.io.FileOutputStream
 
 class PicRepositoryImpl(val context : Context) : PicRepository {
     override fun savePic(uri: Uri, fileName: String) {
+        if (getPic(fileName) == uri){
+            return
+        }
         val filePath = File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "myalbum")
 
-
-        if (!filePath.exists()){
+        if (!filePath.exists()) {
             filePath.mkdirs()
         }
 
         val file = File(filePath, fileName)
 
+
+
         val inputStream = context.contentResolver.openInputStream(uri)
 
         val outputStream = FileOutputStream(file)
 
-        BitmapFactory
-            .decodeStream(inputStream)
-            .compress(Bitmap.CompressFormat.JPEG, 30, outputStream)
+        val bitmap = BitmapFactory.decodeStream(inputStream)
+
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 30, outputStream)
 
 
-
-
-//        context.contentResolver.openInputStream(uri)?.use { inputStream ->
-//            FileOutputStream(file).use { outputStream ->
-//                BitmapFactory.decodeStream(inputStream)
-//                    ?.compress(Bitmap.CompressFormat.JPEG, 30, outputStream)
-//            }
-//        }
+        inputStream?.close()
+        outputStream.close()
     }
 
     override fun getPic(fileName: String): Uri? {
@@ -46,5 +45,13 @@ class PicRepositoryImpl(val context : Context) : PicRepository {
         val file = File(filePath, fileName)
 
         return file.toUri() ?: null
+    }
+    override fun deletePic(fileName: String) {
+        val filePath = File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "myalbum")
+        val file = File(filePath, fileName)
+
+        if (file.exists()) {
+            file.delete()
+        }
     }
 }
